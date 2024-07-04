746a930d452f8a254d69ae13e8b66e9f04771af9
Danylo Skepskyi <kol230305@gmail.com>
2024-07-04 15:19:48 +0200

    implement russian language

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
 
-def print_hello_world(text: str = "nothing") -> None:
+def print_english(text: str) -> None:
     printed_text = ""
 
     for j in range(len(text)):
@@ -19,7 +19,50 @@ def print_hello_world(text: str = "nothing") -> None:
                 break
 
 
+def print_russian(text: str) -> None:
+    printed_text = ""
+
+    for j in range(len(text)):
+        time.sleep(0.1)
+        current_char = text[j]
+        for i in range(1072, 1103 + 1):
+            time.sleep(0.05)
+            letter = chr(i)
+            print(printed_text + letter)
+            if letter == current_char:
+                printed_text += letter
+                break
+            elif current_char == ' ':
+                printed_text += ' '
+                break
+
+
+
+
+
 if __name__ == "__main__":
-    text = (input("Enter Text:"))
+    while True:
+        print("Choose Language:")
+        time.sleep(0.1)
+        print("English: E")
+        time.sleep(0.1)
+        print("Russian: R")
+        time.sleep(0.1)
+        language = input("E/R: ")
+        time.sleep(0.1)
+        if language in ["R", "r", "Russian", "Rus", "rus", "russian", "E", "e", "English", "english", "eng", "Eng"]:
+            break
+        print("WRONG DATA")
+        time.sleep(0.1)
+    while True:
+        text_to_print = (input("Enter Text: "))
+        if text_to_print:
+            break
+        print("ENTER TEXT!")
+    if language in ["R", "r", "Russian", "Rus", "rus", "russian"]:
+        print_russian(text_to_print)
+    elif language in ["E", "e", "English", "english", "eng", "Eng"]:
+        print_english(text_to_print)
+
     
-    print_hello_world(text)
+
